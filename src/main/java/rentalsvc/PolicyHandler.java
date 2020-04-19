f7a562a9c8b9c0c65c03122aa646bb49721b9143
pdc <condolp7@gmail.com>
2020-04-19 17:06:23 +0900

    CQRS 적용

--- src/main/java/rentalsvc/PolicyHandler.java
+++ src/main/java/rentalsvc/PolicyHandler.java
@@ -3,6 +3,8 @@ package rentalsvc;
 import rentalsvc.config.kafka.KafkaProcessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,13 +12,51 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
+
     
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverRentalRequestOccured_ChargeFree(@Payload RentalRequested rentalRequested){
+
+        if(rentalRequested.isMe()){
+            System.out.println("##### listener  랜트요청 이벤트 수신(RentalRequested) : " + rentalRequested.toJson());
+            
+           FeeReceived feeReceived = new FeeReceived();
+            
+	           feeReceived.setOrderId(rentalRequested.getOrderId());
+	           feeReceived.setAmount(rentalRequested.getAmount());
+	           feeReceived.publish();
+	            
+           System.out.println("##### 요금수납 완료 이벤트 발송 : (FeeReceived)");
+        }
+    }
+	
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverRentalCancellationOccured_FreeRefund(@Payload RentalCancellationOccured rentalCancellationOccured){
 
         if(rentalCancellationOccured.isMe()){
-            System.out.println("##### listener FreeRefund : " + rentalCancellationOccured.toJson());
+            System.out.println("##### listener  환불요 이벤트 수신(RentalCancellationOccured) : " + rentalCancellationOccured.toJson());
+            
+            FeeRefundCompleted feeRefundCompleted = new FeeRefundCompleted();
+            
+            feeRefundCompleted.setOrderId(rentalCancellationOccured.getOrderId());
+            feeRefundCompleted.publish();
+            
+           System.out.println("##### 환불처리 완료 이벤트 발송 : (FeeRefundCompleted)");
         }
     }
 
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheninventoryStockShortageOccured_FreeRefund(@Payload InventoryStockShortage inventoryStockShortage){
+
+        if(inventoryStockShortage.isMe()){
+            System.out.println("##### listener  재고부족 이벤트 수신(InventoryStockShortage) : " + inventoryStockShortage.toJson());
+            
+            FeeRefundCompleted feeRefundCompleted = new FeeRefundCompleted();
+            
+            feeRefundCompleted.setOrderId(inventoryStockShortage.getOrderId());
+            feeRefundCompleted.publish();
+            
+           System.out.println("##### 환불처리 완료 이벤트 발송 : (FeeRefundCompleted)");
+        }
+    }
 }
